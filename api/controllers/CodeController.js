b13d047882ecd35ecccdccd7c99e675a8f2f334f
udany <long.eagle.goodies@gmail.com>
2018-09-07 20:02:34 -0300

    Code/User managers

--- api/controllers/CodeController.js
+++ api/controllers/CodeController.js
@@ -5,14 +5,48 @@ import csv from 'csvtojson';
 const dataFolder = path.resolve('./data');
 
 const codeListFile = path.resolve(dataFolder, 'codes.csv');
-let codes = [];
+let codes = [], unassignedCodes = [];
+
+const usersFile = path.resolve(dataFolder, 'users.json');
+let users = [];
+
+const userManager = {
+	start() {
+		if (!fs.existsSync(usersFile)) {
+			this.createFile();
+		} else {
+			this.loadFile();
+		}
+	},
+	createFile() {
+		fs.writeFileSync(usersFile, '[]');
+	},
+	loadFile() {
+		let data = fs.readFileSync(usersFile);
+		users = JSON.parse(data);
+	},
+	saveFile() {
+		fs.writeFileSync(usersFile, JSON.stringify(users));
+	}
+};
+
+const codeManager = {
+	start() {
+		userManager.start();
+		this.loadCodes();
+	},
+	async loadCodes() {
+		codes = await csv().fromFile(codeListFile);
+		unassignedCodes = codes.filter(code => !users.find(user => user.code === code.code));
+	}
+};
 
 /**
  * @name CodeController
  */
 export default {
-	getUnassignedCode() {
-
+	start() {
+		codeManager.start();
 	},
 	getUserCode() {
 
@@ -20,10 +54,10 @@ export default {
 	assignCodeToUser() {
 
 	},
-	async loadCodes() {
-		codes = await csv().fromFile(codeListFile);
-	},
 	allCodes() {
 		return codes;
-	}
+	},
+	unassignedCodes() {
+		return unassignedCodes;
+	},
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import CodeController from '../controllers/CodeController';
 
+CodeController.start();
+
+
 let router = express.Router({});
 
 router.get('/', async function (req, res, next) {
-	await CodeController.loadCodes();
-
 	res.send(CodeController.allCodes());
 });
 
