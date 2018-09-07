be7eaff6c20e9271215c0311ce08e60cb5c721f3
udany <long.eagle.goodies@gmail.com>
2018-09-07 16:55:45 -0300

    No database for u

--- api/config/config.development.js
+++ api/config/config.development.js
@@ -2,13 +2,6 @@ let gCfg = require('./config.global');
 
 let cfg = JSON.parse(JSON.stringify(gCfg));
 
-// cfg.mysql = {
-// 	host: 'localhost',
-// 	user: 'root',
-// 	password: 'admin',
-// 	database: 'hackatinder'
-// };
-
 // Changes go here
 
 export default cfg;
--- api/config/config.global.js
+++ api/config/config.global.js
@@ -1,12 +1,6 @@
 let cfg = {
     port: 3001,
-    debug: true,
-    mysql: {
-        host: '174.138.47.54',
-        user: 'unirio',
-        password: 'KE8Aa8rw55sQdxas',
-        database: 'hackatinder'
-    }
+    debug: true
 };
 
 module.exports = cfg;
